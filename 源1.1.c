9de513bb23c29777af4d78a19ae32a073e7ba2ff
Crazyinfy32 <86730481+Crazyinfy32@users.noreply.github.com>
2021-07-29 10:14:54 -0400

    Add files via upload

---
+++ 源1.1.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void wife(int num)
+//{
+//	if (num > 9)
+//	{
+//		wife(num / 10);
+//	}
+//	printf("%d ", num % 10);
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);
+//	wife(num);
+//	return 0;
+//}
+#include<string.h>
+int main()
+{
+	char arr[] = "bit";
+	int len = strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
